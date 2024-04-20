7ea4a39b9c9c2492ae51ce4fa231f15a2c8f8678
crazydevlegend <crazy010323@gmail.com>
2024-04-19 20:22:29 -0400

    feat: add NicheImage(SN23) to the backend

--- server.py
+++ server.py
@@ -9,6 +9,7 @@ from crossvals.image_aichemy.aichemy import ImageAIchemyCrossVal
 from crossvals.sybil.sybil import SybilCrossVal
 from crossvals.openkaito.openkaito import OpenkaitoCrossVal
 from crossvals.itsai.itsai import ItsaiCrossVal
+from crossvals.niche.niche import NicheCrossVal
 
 from fastapi import UploadFile, File, HTTPException
 import asyncio
@@ -60,6 +61,9 @@ class OpenkaitoItem(BaseModel):
 class ItsaiItem(BaseModel):
     texts: List[str]
 
+class NicheItem(BaseModel):
+    model_name: str
+
 @app.get("/")
 def read_root():
     return translate_crossval.run("Hello, how are you?")
@@ -87,6 +91,10 @@ async def openkaito_search(item: OpenkaitoItem):
 @app.post("/itsai/")
 async def llm_detection(item: ItsaiItem):
     return await itsai_crossval.run(item.texts)
+
+@app.post("/niche/")
+def niche_generation(item: NicheItem):
+    return niche_crossval.run(item)
 
 class ImageUpload(BaseModel):
     file: UploadFile = File(...)
@@ -138,3 +146,4 @@ imageaichemy_crossval = ImageAIchemyCrossVal()
 sybil_crossval = SybilCrossVal()
 openkaito_crossval = OpenkaitoCrossVal()
 itsai_crossval = ItsaiCrossVal()
+niche_crossval = NicheCrossVal()
