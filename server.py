a43d364dd59ce685638fe22c5857ef9675fd0ffc
root <root@vmi1736426.contaboserver.net>
2024-04-26 08:54:26 -0400

    feat: add Bitagent(SN20) to the backend

--- server.py
+++ server.py
@@ -17,6 +17,7 @@ from crossvals.audiogen.audiogen import AudioGenCrossVal
 from crossvals.llm_defender.llm_defender import LLMDefenderCrossVal
 from crossvals.transcription.transcription import TranscriptionCrossVal
 from crossvals.subvortex.subvortex import SubvortexCrossVal
+from crossvals.bitagent.bitagent import BitagentCrossVal
 
 from fastapi import UploadFile, File, HTTPException, Body
 import asyncio
@@ -145,6 +146,10 @@ def transcription(item: TranscriptionItem):
 async def subvortex_calc():
     return await subvortex_crossval.run()
 
+@app.post("/bitagent/")
+def bitagent():
+    return bitagent_crossval.run()
+
 class ImageUpload(BaseModel):
     file: UploadFile = File(...)
 
@@ -202,3 +207,4 @@ audiogen_crossval = AudioGenCrossVal()
 llmdefender_crossval = LLMDefenderCrossVal()
 transcription_crossval = TranscriptionCrossVal()
 subvortex_crossval = SubvortexCrossVal()
+bitagent_crossval = BitagentCrossVal()
